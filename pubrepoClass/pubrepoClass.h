2577ccc865c47dcb95a4f026fc23b60992a75002
QITMAC000578 <zuobin.chen@qunar.outlook.com>
2019-02-20 10:32:51 +0800

    单独函数

---
+++ pubrepoClass/pubrepoClass.h
@@ -0,0 +1,21 @@
+//
+//  pubrepoClass.h
+//  testpubrepo
+//
+//  Created by QITMAC000578 on 2019/2/19.
+//  Copyright © 2019 QITMAC000578. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface pubrepoClass : NSObject
+
++(void)testMethod;
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
